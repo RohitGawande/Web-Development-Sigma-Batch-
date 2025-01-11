b8e2f2cb1b464f6f21c5ad8e3e9dbaf8a6942d62
Rohit Gawande <rohitgawande2004@gmail.com>
2025-01-11 15:28:11 +0530

    JavaScript

---
+++ 7.JavaScript(Part 6)/3.Practise Qs/Create a function that prints a poem/Example.js
@@ -0,0 +1,6 @@
+//Create a function that prints a poem 
+function printPoem() {
+    console.log("Twinkle Twinkle,little Star");
+    console.log("how I wonder what you are");
+}
+printPoem();
---
+++ 7.JavaScript(Part 6)/3.Practise Question 2/Example.js
@@ -0,0 +1,5 @@
+//Create a Function to roll a dice and always display the value of the dice ( 1 to 6).
+function randomNum(){
+    console.log(Math.floor(Math.random()*6));
+}
+randomNum();
